1d550204ea2f7297c79350908e62fc139cb9f8fb
Lorenz Hübschle-Schneider <lorenz-dev@lgh-alumni.de>
2015-03-22 18:40:16 +0100

    Implement dynamic programming levenshtein distance with matrix

--- src/libfuzzymatch/levenshtein.cpp
+++ src/libfuzzymatch/levenshtein.cpp
@@ -1,8 +1,24 @@
+#include <algorithm>
+
+#include "levenshtein.h"
 
 /**
- * Levenshtein distance for ASCII strings.
+ * Levenshtein distance for UTF-32 "strings".
  */
-int levenshtein(const char* s, const char* t)
-{
-    return 0;
+uint32_t levenshtein(const std::vector<uint32_t> &s, const std::vector<uint32_t> &t) {
+	const size_t n(s.size()), m(t.size());
+	std::vector<uint32_t> current(m + 1, 0), last(m + 1);
+
+	// Initialise with unmatched characters in t
+	for (size_t j = 0; j <= m; ++j) last[j] = j;
+
+	for (size_t i = 0; i < n; ++i) {
+		current[0] = i+1; // = last[0]+1, corresponds to unmatched character in s
+		for (size_t j  = 1; j <= m; ++j) {
+			current[j] = std::min(last[j] + 1, current[j-1] + 1); // insertion / deletion
+			current[j] = std::min(current[j], last[j-1] + (int)(s[i] != t[j-1])); // modification
+		}
+		current.swap(last);
+	}
+    return last.back();
 }
---
+++ src/libfuzzymatch/levenshtein.h
@@ -0,0 +1,7 @@
+#include <cstdint>
+#include <vector>
+
+/**
+ * Levenshtein distance for UTF-32 "strings".
+ */
+uint32_t levenshtein(const std::vector<uint32_t> &, const std::vector<uint32_t> &);
