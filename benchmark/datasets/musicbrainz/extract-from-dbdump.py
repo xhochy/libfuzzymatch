fe63d6e1e822f7cb60d1c0bdaa08eb53d3849783
Uwe L. Korn <uwelk@xhochy.com>
2015-03-22 17:31:25 +0100

    Add script to extract artist names from MusicBrainz database

---
+++ benchmark/datasets/musicbrainz/extract-from-dbdump.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""
+Script to extract the artist names from a MusicBrainz database dump.
+
+Usage:
+    ./extract-from-dbdump.py <dump_dir>/artist <outfile>
+"""
+
+import pandas as pd
+import sys
+
+__author__ = "Uwe L. Korn"
+__license__ = "MIT"
+
+
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+df = pd.read_csv(input_file, sep='\t', header=None)
+df.ix[:, 2].to_csv(outfile, index=False)
