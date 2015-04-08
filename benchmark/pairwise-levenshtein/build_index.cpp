11f8f65d024fbd1e4e36ec1dfbd9bb8ebadeae8b
Uwe L. Korn <uwelk@xhochy.com>
2015-04-08 22:10:59 +0200

    Implement index building for pairwise levenshtein

--- benchmark/pairwise-levenshtein/build_index.cpp
+++ benchmark/pairwise-levenshtein/build_index.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -6,4 +7,12 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
+    std::ifstream file(argv[argc - 1]);
+    std::ofstream index("index.plain");
+    index << file.rdbuf();
+    file.close();
+    index.close();
+
+    // Only I/O which does not count towards measured time.
+    std::cout << "Took 0s to build an index." << std::endl;
 }
