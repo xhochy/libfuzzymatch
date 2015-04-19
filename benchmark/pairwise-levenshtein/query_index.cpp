1115f0e5b3b2778bbf441992d07b9ba4a1de5fe4
Uwe L. Korn <uwelk@xhochy.com>
2015-04-19 19:07:07 +0200

    Only insert into the PQ if actually better than current worst

--- benchmark/pairwise-levenshtein/query_index.cpp
+++ benchmark/pairwise-levenshtein/query_index.cpp
@@ -19,9 +19,17 @@ void benchLevenshtein(std::vector<std::string> queries,
         std::priority_queue<std::tuple<uint32_t, size_t>> pq;
         for (size_t idx = 0; idx < index.size(); idx++) {
             uint32_t distance = lv(query, index[idx]);
-            pq.push(std::tuple<uint32_t, size_t>(distance, idx));
-            if (pq.size() > 20) {
-                pq.pop();
+            if (pq.size() < 20) {
+                // Less than 20 elements, we can just push it into the queue.
+                pq.push(std::tuple<uint32_t, size_t>(distance, idx));
+            } else {
+                // There are 20 element in the PriorityQueue. Check if the
+                // element should go into the queue but we should first remove
+                // the weakest element so that the queue is a bit smaller.
+                if (std::get<0>(pq.top()) > distance) {
+                    pq.pop();
+                    pq.push(std::tuple<uint32_t, size_t>(distance, idx));
+                }
             }
         }
     }
