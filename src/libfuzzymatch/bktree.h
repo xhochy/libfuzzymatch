b5e73f1bb29bd8fcb0dcfb39d5d3994d128e5e12
Uwe L. Korn <uwelk@xhochy.com>
2015-04-19 19:32:48 +0200

    Document the BKTree implementation

--- src/libfuzzymatch/bktree.h
+++ src/libfuzzymatch/bktree.h
@@ -17,8 +17,21 @@ public:
     BKTree(size_t n, score_func_type score_func) :
         m_tree(n), m_score_func(score_func) { }
 
+    /**
+     * Insert a new element in the tree.
+     *
+     * This will compute the scoring function several times to find the correct
+     * place in the tree. A copy of the input vector will be stored in the tree.
+     *
+     * @param input The new element.
+     */
     void insert(const std::vector<input_type> &input) {
+        // At the moment we only support adding as much elements as were
+        // specified on construction. We might easily lift this restriction
+        // but this will come at the moment with the drawback of copying the
+        // entire tree which is a very expensive operation.
         assert(m_next_free < m_tree.size());
+
         if (m_next_free == 0) {
             m_tree[0].first = input;
             m_next_free++;
@@ -28,6 +41,22 @@ public:
     }
 
 protected:
+    /**
+     * Recurse through the tree to find the correct position to insert the new element.
+     *
+     * To insert an element into the tree, we compute the distance of the new
+     * element to the currently traversed node. If they have a distance of 0
+     * the new element is considered as equal and will not be inserted into the tree.
+     * For a distance >0, we will look at the outgoing edge labelled with the given
+     * score. If there is no such edge (either the list of edges is smaller
+     * than the score or at the position we have a value of -1) then we will insert
+     * a new edge and connect it to the new node. In the other case where the
+     * edge matching the distance score already exists, we will recursively call
+     * the `recursive_insert` function with this edge and continue the algorithm.
+     *
+     * @param input The new element.
+     * @param current_node The current node which is checked as a candidate for being the direct parent.
+     */
     void recursive_insert(const std::vector<input_type> &input, size_t current_node) {
         score_type score = m_score_func(m_tree[current_node].first, input);
         if (score == 0) {
@@ -51,8 +80,34 @@ protected:
     }
 
 private:
+    /**
+     * The index of the next available position of a node in the internal tree
+     * representation `m_tree`.
+     */
     size_t m_next_free = 0;
+
+    /**
+     * Function that is called to compute the distance between two elements. As
+     * a requirement for the BK-Tree's correct function, this needs to be a
+     * metric distance function.
+     */
     score_func_type m_score_func;
+
+    /**
+     * Internal tree representation.
+     *
+     * Each entry is the pair of the value at this node (e.g. a UTF-32 string)
+     * and the list of outgoing edges. The list of edges is implemented as a
+     * not fully "used" array. If an edges exists for a score D, the entry at
+     * position [D - 1] points to the index in m_tree where the target node
+     * resides. Empty entries in this array are marked with a value of -1
+     * (as this is not a valid value for an array index in C++).
+     *
+     * For the sake of performance, the outer std::vector should be initialised
+     * on the instantiation of the BKTree class once and not resized during
+     * building as this would invoke a very costly copy operation of the whole
+     * tree.
+     */
     std::vector<std::pair<std::vector<input_type>, std::vector<int64_t>>> m_tree;
 };
 
