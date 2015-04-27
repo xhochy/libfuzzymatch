14c086161016592b4b756826c65d96a2ed28d057
Lorenz Hübschle-Schneider <lorenz-dev@lgh-alumni.de>
2015-04-27 20:06:33 +0200

    limit search space to (2t+1) max distance corridor
    use full data structure

--- src/libfuzzymatch/levenshtein.h
+++ src/libfuzzymatch/levenshtein.h
@@ -22,3 +22,27 @@ uint32_t levenshtein(const std::vector<uint32_t> &, const std::vector<uint32_t> 
  * @return The levenshtein distance between the two inputs.
  */
 uint32_t levenshteinStatic(const std::vector<uint32_t> &s, const std::vector<uint32_t> &t);
+
+/**
+ * Levenshtein distance for UTF-32 "strings" with maximum distance parameter.
+ * @param s First string input for levenshtein distance
+ * @param t Second string input for levenshtein distance
+ * @param threshold Maximum Levenshtein distance between s and t before aborting
+ * @return The Levenshtein distance between the two inputs, or -1 if it exceeds the threshold.
+ */
+uint32_t levenshteinLimit(const std::vector<uint32_t> &, const std::vector<uint32_t> &, const uint32_t);
+
+/**
+ * Limited Levenshtein distance for UTF-32 "strings" using static storage for less allocations.
+ *
+ * The two temporary used arrays are statically allocated internally, i.e. are
+ * always expanded to the maximum size of the second input and never scaled down.
+ * Although this offers better performance, you need to be aware that large
+ * inputs will increase the memory usage for the whole lifespan of the procress.
+ *
+ * @param s First string input for levenshtein distance
+ * @param t Second string input for levenshtein distance
+ * @param threshold Maximum Levenshtein distance between s and t before aborting
+ * @return The Levenshtein distance between the two inputs, or -1 if it exceeds the threshold.
+ */
+uint32_t levenshteinLimitStatic(const std::vector<uint32_t> &, const std::vector<uint32_t> &, const uint32_t);
