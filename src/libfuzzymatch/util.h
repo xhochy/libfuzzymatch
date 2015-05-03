bb97296d4b4285e6884e61ceda833af5a453cb91
Uwe L. Korn <uwelk@xhochy.com>
2015-05-03 18:02:57 +0200

    Use inline as a workaround so that the utf8 functions are not exported

--- src/libfuzzymatch/util.h
+++ src/libfuzzymatch/util.h
@@ -8,7 +8,7 @@
 /*
  * Convert UTF-8 string to std::vector<uint32_t>
  */
-void utf8to32(const std::string &s, std::vector<uint32_t> &vec) {
+void inline utf8to32(const std::string &s, std::vector<uint32_t> &vec) {
 	vec.assign(utf8::distance(s.cbegin(), s.cend()), 0);
 	utf8::utf8to32(s.cbegin(), s.cend(), vec.data());
 }
@@ -16,7 +16,7 @@ void utf8to32(const std::string &s, std::vector<uint32_t> &vec) {
 /*
  * Convert UTF-8 C-string to std::vector<uint32_t>
  */
-void utf8to32(char* const s, std::vector<uint32_t> &vec) {
+void inline utf8to32(char* const s, std::vector<uint32_t> &vec) {
 	const size_t len(strlen(s));
 	vec.assign(utf8::distance(s, s+len), 0);
 	utf8::utf8to32(s, s+len, vec.data());
